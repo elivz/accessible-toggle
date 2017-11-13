31557f9292e5e87021a6f6f78d582291a19b7fed
Eli Van Zoeren <eli@elivz.com>
2017-11-13 10:53:47 -0500

    Update Rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import babel from 'rollup-plugin-babel';
 import nodeResolve from 'rollup-plugin-node-resolve';
 import uglify from 'rollup-plugin-uglify';
-import bundleSize from 'rollup-plugin-bundle-size';
+import filesize from 'rollup-plugin-filesize';
 import commonjs from 'rollup-plugin-commonjs';
 
 const name = `accessibleToggle`;
@@ -10,21 +10,23 @@ const plugins = [
   babel(),
   nodeResolve({
     module: true,
-    jsnext: true
+    jsnext: true,
   }),
   commonjs({
-    include: `node_modules/**`
+    include: `node_modules/**`,
   }),
-  bundleSize()
+  filesize(),
 ];
 
 const isProd = process.env.NODE_ENV === `production`;
 if (isProd) plugins.push(uglify());
 
 export default {
-  entry: `src/index.js`,
+  input: `src/index.js`,
   plugins,
-  dest: `dist/${name}${isProd ? `.min` : ``}.js`,
-  moduleName: name,
-  format: `umd`
+  output: {
+    file: `dist/${name}${isProd ? `.min` : ``}.js`,
+    name: name,
+    format: `umd`,
+  },
 };
